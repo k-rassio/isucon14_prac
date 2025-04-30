b35b10611abee27265cdf2521c0f0ed208aa67d6
k-rassio <igarashi4948@gmail.com>
2025-04-30 10:44:06 +0000

    first commit

---
+++ push_and_build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eux
+
+isu_repo_dir="/home/user/isucon14"
+isu_webapp_dir="./webapp/"
+isu_ansible_dir="/home/user/isucon14/provisioning/ansible"
+webapp_tar_dir="/home/user/isucon14/provisioning/ansible/roles/webapp/files/webapp.tar.gz"
+
+cd $isu_repo_dir
+
+echo "############# git commit & push ###############"
+# git add . || true
+# git commit -m "$1_`date "+%Y_%m_%d_%H_%M_%S"`" || true
+git push -u origin || true
+
+echo "############ appサーバへのデプロイ ################"
+tar -zcvf $webapp_tar_dir $isu_webapp_dir
+cd $isu_ansible_dir
+ansible-playbook -i inventory/hosts.yaml -l isu14_app application-deploy.yml
+
+echo "################ benchの実行 ####################"
+ansible -i inventory/hosts.yaml isu14_bench -b --become-user isucon -a "/home/isucon/bench_linux_amd64 run ./bench_linux_amd64 run --target=https://192.168.37.134:443 --payment-url=https://192.168.37.131:12345" | tee /home/user/isucon14/result/bench_result/bench_`date +%m%d%H%M`.log
+
+echo "################ benchの結果を取得 ####################"
+
+echo "############### mysqldumpの実行 #################"
+ansible-playbook -i inventory/hosts.yaml -l isu14_app mysql_slowquery.yml
+echo "################# alpの実行 #####################"
+ansible-playbook -i inventory/hosts.yaml -l isu14_app alp.yml
