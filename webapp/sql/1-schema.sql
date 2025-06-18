0b6353111d5b0a49f5be959f6f7e87994b4bf76e
k-rassio <igarashi4948@gmail.com>
2025-06-18 09:53:05 +0000

    add chair table to is_active index

--- webapp/sql/1-schema.sql
+++ webapp/sql/1-schema.sql
@@ -37,6 +37,7 @@ CREATE TABLE chairs
   COMMENT = '椅子情報テーブル';
 ALTER TABLE chairs ADD INDEX idx_owner_id(owner_id);
 ALTER TABLE chairs ADD INDEX idx_access_token(access_token);
+ALTER TABLE chairs ADD INDEX idx_is_active(is_active);
 
 DROP TABLE IF EXISTS chair_locations;
 CREATE TABLE chair_locations
