ea7beef7d299be60794de0be1279a873ecb7e9c6
k-rassio <igarashi4948@gmail.com>
2025-06-18 10:18:30 +0000

    add index

--- webapp/sql/1-schema.sql
+++ webapp/sql/1-schema.sql
@@ -112,6 +112,7 @@ CREATE TABLE ride_statuses
 )
   COMMENT = 'ライドステータスの変更履歴テーブル';
 ALTER TABLE ride_statuses ADD INDEX idx_ride_id_created_at(ride_id, created_at);
+ALTER TABLE ride_statuses ADD INDEX idx_ride_statuses_ride_chair_created (ride_id, chair_sent_at, created_at);
 
 DROP TABLE IF EXISTS owners;
 CREATE TABLE owners
